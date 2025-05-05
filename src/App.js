849de64cb7662ee00068866048789384c677ebe6
Robert George Leustean <147397130+914RobbLeustean@users.noreply.github.com>
2025-05-05 04:32:44 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/css/App.css';
+import VacationExpenseTracker from './components/VacationExpenseTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <VacationExpenseTracker />
+    </div>
+  );
+}
+
+export default App;
